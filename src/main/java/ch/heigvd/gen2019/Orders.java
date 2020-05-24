21d2246f880f5e2ae8254bccf0b8374d625647ea
alfavre <fav.alban@gmail.com>
2020-05-24 21:18:07 +0200

    bad smell: method in wrong class, refactor: move method getContents in Orders

--- src/main/java/ch/heigvd/gen2019/Orders.java
+++ src/main/java/ch/heigvd/gen2019/Orders.java
@@ -17,4 +17,33 @@ public class Orders {
     public Order getOrder(int i) {
         return orders.get(i);
     }
+
+    public String getContents() {
+        StringBuffer sb = new StringBuffer("{\"orders\": [");
+
+        for (int i = 0; i < getOrdersCount(); i++) {
+            Order order = getOrder(i);
+            sb.append("{");
+            sb.append("\"id\": ");
+            sb.append(order.getOrderId());
+            sb.append(", ");
+            sb.append("\"products\": [");
+            for (int j = 0; j < order.getProductsCount(); j++) {
+                order.getProduct(j).getProductContents(sb);
+            }
+
+            if (order.getProductsCount() > 0) {
+                sb.delete(sb.length() - 2, sb.length());
+            }
+
+            sb.append("]");
+            sb.append("}, ");
+        }
+
+        if (getOrdersCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        return sb.append("]}").toString();
+    }
 }
--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -1,39 +1,10 @@
 package ch.heigvd.gen2019;
 
 public class OrdersWriter {
-    private Orders orders;
+    Orders orders;
 
     public OrdersWriter(Orders orders) {
         this.orders = orders;
-    }
-
-    public String getContents() {
-        StringBuffer sb = new StringBuffer("{\"orders\": [");
-
-        for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                order.getProduct(j).getProductContents(sb);
-            }
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
-            sb.append("}, ");
-        }
-
-        if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        return sb.append("]}").toString();
     }
 
 }
